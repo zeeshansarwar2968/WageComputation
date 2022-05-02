7384203c6b43ac594914ba500821495f2a4e8e4c
Zeeshan Sarwar <zeeshansarwar2968@gmail.com>
2022-05-02 18:45:53 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace WageComputation // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Wage Computation");
+        }
+    }
+}
